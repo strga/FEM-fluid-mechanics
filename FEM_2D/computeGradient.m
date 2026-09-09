9338758c98b18d07b5979d6dec91f3205d1298c2
strga <151577860+strga@users.noreply.github.com>
2026-09-10 01:43:03 +0200

    Update computeGradient.m

--- FEM_2D/computeGradient.m
+++ FEM_2D/computeGradient.m
@@ -1,6 +1,6 @@
 function RR = computeGradient(K)
 
-GH = [-1 -1; 1 0; 0 1]; % Derivace bazove funkce na referencnim elementu
+GH = [-1 -1; 1 0; 0 1]; % Derivatives of basis function on reference element
 
 RR = (GH * K.inBB)'; % Matrix 2x3, 
 
