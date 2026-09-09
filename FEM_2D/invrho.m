5a546a730b833ef84ddd33104efd3c4bf20e0133
strga <151577860+strga@users.noreply.github.com>
2026-09-10 01:37:29 +0200

    Update invrho.m

--- FEM_2D/invrho.m
+++ FEM_2D/invrho.m
@@ -3,10 +3,10 @@
 kPa = 1e3;
 rho0 = 1.225;
 p0 = 101.325 * kPa;
-gamma = 1.4; % 2 atom idealni plyn - Poisson constant
+gamma = 1.4; % 2 atom ideal gass - Poisson constant
 a0sqrt = gamma * p0 / rho0;
 velsqrt = U(1) * U(1) + U(2) * U(2);
 
 gam1 = gamma - 1;
-ret = ( (1 - 1. / 2. * gam1 / a0sqrt * velsqrt ./ rho ./ rho).^gam1 / rho0 ); %Vztah pro inverse density z Beroulliho vzorce
+ret = ( (1 - 1. / 2. * gam1 / a0sqrt * velsqrt ./ rho ./ rho).^gam1 / rho0 ); %Inverse density - Bernoulli
 end
