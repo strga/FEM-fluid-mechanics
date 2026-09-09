31a3eca9b075805a67f53d8f6ed14e2c3949eb10
strga <151577860+strga@users.noreply.github.com>
2026-09-10 01:42:30 +0200

    Update computeIntegral1.m

--- FEM_2D/computeIntegral1.m
+++ FEM_2D/computeIntegral1.m
@@ -6,10 +6,7 @@ function s = computeIntegral1(M, f) %Těžiště
 
         K = getMeshElement(M, k);
 
-        %Sab = (K.A + K.B)/2;
-
         T = (K.A + K.B + K.C)/3;
-        %T = Sab + 1/3 * (K.C - Sab);
 
         s = s + K.vol*f(T(1),T(2));
     end
